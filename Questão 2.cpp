b9eb20ef031d08eff0bb811cdee1c8fcdc3c7d42
kauanINFO <klogue2009@gmail.com>
2025-07-09 15:09:21 -0300

    Create Questão 2.cpp

---
+++ Questão 2.cpp
@@ -0,0 +1,18 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+   double n1;
+   double n2;
+   cout << "Digite dois numeros: ";
+   cin >> n1 >> n2;
+   double soma = n1 + n2;
+   double  sub = n1 - n2;
+   double multi =  n1 * n2;
+   double divi = n1 / n2;
+   cout << soma << "\n" << sub << "\n" << multi << "\n" << divi << endl;
+}
