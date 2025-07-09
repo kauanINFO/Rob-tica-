314df4c6bd69021a69385a6f3c4a9ba3286a8115
kauanINFO <klogue2009@gmail.com>
2025-07-09 15:51:18 -0300

    Add files via upload

---
+++ questa23/fucoes.h
@@ -0,0 +1,9 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include <iostream>
+
+// Declaração da função
+void mostrarInfo();
+
+#endif // FUNCOES_H
---
+++ questa23/main.cpp
@@ -0,0 +1,11 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "funcoes.h"
+
+int main() {
+    mostrarInfo();
+    return 0;
+}
