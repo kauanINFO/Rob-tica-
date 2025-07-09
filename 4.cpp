83e4d45619a8d8fe54880491438493a80850d471
kauanINFO <klogue2009@gmail.com>
2025-07-09 15:22:08 -0300

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+   int n1 =  5;
+   float n2 = 5;
+   double n3 = 0.5;
+   int demos = n1 / 2;
+   float demos2 = n2 / 2;
+   double demos3 = n3 / 2;
+   cout << demos << "\n" << demos2 << "\n" << demos3 << endl;
+   //Obs a demostração é a seguinte o resultado demostra pq o primeiro valor é inteiro o segundo por float tem uma casa decimal e o terceiro valor por ser double tem quantas casas decimais poder so que no exemplo eu so cloquei duas
+}
