64c3eb4e6ec1ac3bab32929f4ac81a1a631f9ac9
kauanINFO <klogue2009@gmail.com>
2025-07-09 15:23:09 -0300

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,15 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+   std:: string nome;
+   int idade ;
+   cout << "Informe seu nome e sua idade: ";
+   cin >> nome >> idade;
+   cout << "Ola " << nome << " com idade " << idade << " Boa tarde" << endl;
+}
+
