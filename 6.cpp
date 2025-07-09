dd24438d37e3eaad4fa6ace7bfe2e77aa101cf47
kauanINFO <klogue2009@gmail.com>
2025-07-09 15:23:45 -0300

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+int soma(int x, int y){
+    int soma = x + y;
+    return soma;
+}
+
+int main()
+{
+   int res = soma(10, 16);
+   cout << res << endl;
+}
+
